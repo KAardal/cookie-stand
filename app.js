7a5fa135bf1b2ca7d425fec929f42ef09688a632
Kyle Aardal <aardal.kyle@gmail.com>
2017-04-10 15:14:29 -0700

    initial creation

---
+++ app.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var stands = [];
+
+var pikeStand = {
+  location: '1st and Pike',
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgPerCustomer: 6.3,
+  cookiesSold: [], //each index will represent an hour in the day starting at 6am
+  
+}
+stands.push(pikeStand);
+
+var seaTacStand = {
+  location: 'SeaTac Airport',
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgPerCustomer: 1.2,
+  cookiesSold: [],
+}
+stands.push(seaTacStand);
+
+var seattleCenterStand = {
+  location: 'Seattle Center',
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgPerCustomer: 3.7,
+  cookiesSold: [],
+}
+stands.push(seattleCenterStand);
+
+var capitolHillStand = {
+  location: 'Capitol Hill',
+  minCustomers: 20,
+  maxCustomers: 38,
+  avgPerCustomer: 2.3,
+  cookiesSold: [],
+}
+stands.push(capitolHillStand);
+
+var alkiStand = {
+  location: 'Alki',
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgPerCustomer: 4.6,
+  cookiesSold: [],
+}
+stands.push(alkiStand);
+
+var listDiv = document.getElementById('list-div');
+var standsUl;
+var listHeading;
+var currentStand;
+for(var i = 0; i < stands.length; i++) {
+  currentStand = stands[i];
+  standsUl = document.createElement('ul');
+  listDiv.appendChild(standsUl);
+  listHeading = document.createElement('lh');
+  standsUl.appendChild(listHeading);
+  listHeading.textContent = currentStand.location;
+
+  var standsLi;
+  var time;
+  var total;
+  var listEntry;
+  for(var j = 0; j < currentStand.cookiesSold.length; j++) {
+    time = j + 6;
+    if(j < 13){
+      listEntry = time + 'am: ';
+    } else {
+      time -= 12;
+      listEntry = time + 'pm: ';
+    }
+
+    listEntry += currentStand.cookiesSold[i] + ' cookies';
+    standsLi = document.createElement('li');
+    standsLi.textContent = listEntry;
+    standsUl.appendChild(standsLi);
+  }
+}
